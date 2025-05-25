7afc68bb67323ab03a62009c193d34214d2e3825
jhalakbhandari <82494331+jhalakbhandari@users.noreply.github.com>
2025-05-25 11:59:49 +0530

    commented out unused code

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,16 +1,16 @@
-import { useDispatch, useSelector } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch } from "./store";
+// import { useDispatch, useSelector } from "react-redux";
+// import type { TypedUseSelectorHook } from "react-redux";
+// import type { RootState, AppDispatch } from "./store";
 export const isAuthenticated = () => {
- const user = localStorage.getItem("user");
+  const user = localStorage.getItem("user");
   if (!user) return false;
   try {
     const parsed = JSON.parse(user);
     return parsed.token && parsed.exp * 1000 > Date.now(); // also checks expiry
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return false;
-}
+  }
 };
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+// export const useAppDispatch = () => useDispatch<AppDispatch>();
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
