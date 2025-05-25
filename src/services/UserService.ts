7b1c399adeef0054466653a9325a79a4418d1de1
jhalakbhandari <82494331+jhalakbhandari@users.noreply.github.com>
2025-05-25 13:05:18 +0530

    added user signup

---
+++ src/services/UserService.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export interface CreateUserPayload {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const handleSignupUser = async (userData: CreateUserPayload) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/api/user/signup`,
+      userData
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error creating user", error);
+    throw error;
+  }
+};
